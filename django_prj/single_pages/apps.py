0a2108eb877193f7ffce37c2b9fd59555f10b97d
JY CHOI <moosou30@naver.com>
2023-03-14 17:43:51 +0900

    blog/Post model 추가

--- django_prj/single_pages/apps.py
+++ django_prj/single_pages/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SinglePagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ddjango_prj.single_pages'
+    name = 'django_prj.single_pages'
--- django_prj/urls.py
+++ django_prj/urls.py
@@ -1,4 +1,4 @@
-"""ddjango_prj URL Configuration
+"""django_prj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
